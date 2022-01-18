58505778029c514072a7a1168aef63f7377aefff
Abrar Shameem <abrarshameem@sazmas-mbp-2.myfiosgateway.com>
2022-01-18 11:14:56 -0500

    first push

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,11 +2,21 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
-         /*
-          Write 40 Fibonacci numbers with java.
-          0,1,1,2,3,5,8,13
-         */
 
+        int dp[] = new int[21];
+        // Initializing fibonacci values for 0th and 1st number
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= 20; i++) {
+            // Using the Fibonacci recurrence relation
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        // Printing Fibonacci numbers
+        for (int i = 0; i <= 20; i ++) {
+            System.out.println("Fibonacci Number for n = " + i + " is " + dp[i]);
+        }
 
     }
+
 }
+
